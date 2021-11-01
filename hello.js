cdb147cc12e538945171f4ad6db297adc8c0cc9b
Mark Chuang <markchuangth@gmail.com>
2021-11-01 08:02:25 -0700

    sayHello function

---
+++ hello.js
@@ -0,0 +1,13 @@
+const sayHello  = function () {
+    console.log("Hello, world");
+  }
+  
+  sayHello();
+
+  const sayHello2  = function (name) {
+    console.log("Hello, " + name);
+  }
+
+sayHello2("Caliban");
+sayHello2("Miranda");
+sayHello2("Ferdinand");
